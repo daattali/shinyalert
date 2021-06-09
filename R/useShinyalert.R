3bc5958becddd5b2e584e271e909ca1439c7fdc8
Dean Attali <daattali@gmail.com>
2021-06-09 14:26:11 -0400

    docs: update wording of useShinyalert force

--- R/useShinyalert.R
+++ R/useShinyalert.R
@@ -2,16 +2,17 @@
 #'
 #' **This function is no longer required.**\cr\cr
 #' The first time a {shinyalert} message is shown, the required scripts are
-#' automatically inserted to the Shiny app. In real browsers (Chrome/Firefox/etc)
-#' this is not an issue, but in some contexts, such as inside RStudio's Viewer
-#' on some operating systems, this can sometimes cause the modal to appear glitchy
-#' for a brief moment until the scripts load.\cr\cr
-#' If you notice this behaviour and prefer to pre-load the scripts when the Shiny
-#' app initializes, you can call `useShinyalert(force=TRUE)` anywhere in the UI.
+#' *automatically* inserted to the Shiny app. Usually this is not an issue, but
+#' in some unique cases this can sometimes cause the modal to appear glitchy
+#' (such as inside RStudio's Viewer, on some old browsers, or if the modal contains
+#' certain Shiny inputs).\cr\cr
+#' If you notice issues with the UI of the modal, you may want to try to pre-load
+#' the scripts when the Shiny app initializes by calling `useShinyalert(force=TRUE)`
+#' anywhere in the UI.
 #'
 #' @param rmd Deprecated, do not use this parameter.
-#' @param force Set to `TRUE` to load the {shinyalert} scripts. If `FALSE` (default),
-#' you will get a warning saying this function is not required.
+#' @param force Set to `TRUE` to force pre-loading the {shinyalert} scripts. If
+#' `FALSE` (default), you will get a warning saying this function is not required.
 #' @return Scripts that \code{shinyalert} requires that are automatically
 #' inserted to the app's \code{<head>} tag.
 #' @examples
@@ -46,7 +47,7 @@ useShinyalert <- function(rmd, force = FALSE) {
   } else {
     warning(
       "Good news!\nYou don't need to call `useShinyalert()` anymore. Please remove this line from your code.\n",
-      "If you really want to pre-load {shinyalert} to the UI for performance reasons, use:\n",
+      "If you really want to pre-load {shinyalert} to the UI for any reason, use:\n",
       "\t`useShinyalert(force = TRUE)`",
       call. = FALSE,
       immediate. = TRUE
