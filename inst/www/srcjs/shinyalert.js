0344b785fdc97de3a3f2acbdef6ce068516a582e
etiennebacher <etienne.bacher@protonmail.com>
2021-08-12 17:53:03 +0200

    add support for mathjax

--- inst/www/srcjs/shinyalert.js
+++ inst/www/srcjs/shinyalert.js
@@ -65,6 +65,9 @@ Shiny.addCustomMessageHandler('shinyalert.show', function(params) {
     }, timer, swal_id);
   }
 
+  // Enable MathJax
+  MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+
   // Modals that need a scrollbar get initialized at the bottom, so let's scroll to top
   $('.sweet-alert')[0].scrollTop = 0;
 });
