fd4894c65e41260a8fb2950bf0bf64c12a46ee1a
etiennebacher <etienne.bacher@protonmail.com>
2021-08-13 11:53:40 +0200

    avoid JS error if mathjax not loaded

--- inst/www/srcjs/shinyalert.js
+++ inst/www/srcjs/shinyalert.js
@@ -66,7 +66,9 @@ Shiny.addCustomMessageHandler('shinyalert.show', function(params) {
   }
 
   // Enable MathJax
-  MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+  if (window.MathJax) {
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
+  }
 
   // Modals that need a scrollbar get initialized at the bottom, so let's scroll to top
   $('.sweet-alert')[0].scrollTop = 0;
