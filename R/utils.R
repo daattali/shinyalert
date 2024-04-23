5060da7710d5170ead47377a16d738cd627767ee
ch0c0l8ra1n <073bct532.rajat@pcampus.edu.np>
2024-04-23 16:53:12 +0545

    Now serving updated minified swalservice

--- R/utils.R
+++ R/utils.R
@@ -23,7 +23,7 @@ getDependencies <- function() {
       version = "1.0.0",
       package = "shinyalert",
       src = "assets/lib/swalservice",
-      script = "swalservice.js"
+      script = "swalservice.min.js"
     ),
     htmltools::htmlDependency(
       name = "shinyalert-binding",
